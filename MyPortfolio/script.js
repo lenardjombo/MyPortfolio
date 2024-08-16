911fd707a7ef660161870fb8191b2438a3527389
Leonard Jombo <158037209+lenardjombo@users.noreply.github.com>
2024-08-15 22:28:33 -0400

    Add files via upload

---
+++ MyPortfolio/script.js
@@ -0,0 +1,24 @@
+const body = document.querySelector("body"),
+      nav = document.querySelector("nav"),
+      sidebarOpen = document.querySelector(".sidebarOpen"),
+      siderbarClose = document.querySelector(".siderbarClose");
+
+      let getMode = localStorage.getItem("mode");
+          if(getMode && getMode === "dark-mode"){
+            body.classList.add("dark");
+          }
+  
+//   js code to toggle sidebar
+sidebarOpen.addEventListener("click" , () =>{
+    nav.classList.add("active");
+});
+
+body.addEventListener("click" , e =>{
+    let clickedElm = e.target;
+
+    if(!clickedElm.classList.contains("sidebarOpen") && !clickedElm.classList.contains("menu")){
+        nav.classList.remove("active");
+    }
+});
+
+
